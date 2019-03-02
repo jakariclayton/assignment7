b181eadd71a1908a16a61eec9fa972241e4096d6
jakariclayton <jclayton1@ufl.edu>
2019-03-01 23:15:05 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+// keep the line above exactly as it is
+
+let bat1 = document.querySelector(".bat2");
+
+bat1.onmouseover = () => {
+  bat1.setAttribute("src","images/baseballbat.jpg");
+}
+
+
+
+
+// // keep the line below exactly as it is
+});
